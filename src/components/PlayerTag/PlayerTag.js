b4c5780df34e82df022f06b8dcd91070d5235396
Alexander Braatz <alex.braatz.g@gmail.com>
2020-09-26 10:42:45 +0100

    Add font Moonhouse

--- src/components/PlayerTag/PlayerTag.js
+++ src/components/PlayerTag/PlayerTag.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { HuePicker  } from 'react-color'; //http://casesandberg.github.io/react-color/
 import Button from '../Button'
+import ColourPicker from '../ColourPicker'
 
 const PlayerTag = ({edit}) => {
 
@@ -19,76 +20,80 @@ const PlayerTag = ({edit}) => {
         e.preventDefault();
     }
 
-return (
-    <>
-    <div className="playerTag__heading">
-        Mario
-    </div>
-   
-        <div className="playerTag__body">
-            <div className="playerTag__body--left">
-                alien
-            </div>
-            <div className="playerTag__body--right">
-                <form className="playerTag__form">
-                    <div className="playerTag__formGroup">
-                        <label 
-                            className="playerTag__lable" 
-                            htmlFor="name"
-                            >Name:
-                        </label>
-                        <input
-                            className="playerTag__nameinput" 
-                            id= "name"
-                            name="edit"
-                            type="text"
+    return (
+        <>
+        <div className="playerTag__heading">
+            Mario
+        </div>
+    
+            <div className="playerTag__body">
+                <div className="playerTag__body--left">
+                    alien
+                </div>
+                <div className="playerTag__body--right">
+                    <form className="playerTag__form">
+                        <div className="playerTag__formGroup">
+                            <label 
+                                className="playerTag__lable" 
+                                htmlFor="name"
+                                >Name:
+                            </label>
+                            <input
+                                className="playerTag__nameinput" 
+                                id= "name"
+                                name="edit"
+                                type="text"
 
-                            onChange={ handleChangeName }
-                            value={ name }
+                                onChange={ handleChangeName }
+                                value={ name }
 
-                        />
-                    </div>
-                    <div className="playerTag__formGroup">
-                        <div 
-                            className="playerTag__lable" 
-                            htmlFor="colour"
-                            >Colour:
+                            />
                         </div>
-                        <div
-                            className="playerTag__colourinput" 
-                            style={ { backgroundColor : colour }} 
+                        <div className="playerTag__formGroup">
+                            <div 
+                                className="playerTag__lable" 
+                                htmlFor="colour"
+                                >Colour:
+                            </div>
+                            {/* <div
+                                className="playerTag__colourinput" 
+                                style={ { backgroundColor : colour }} 
 
-                            onClick={ handleClickColour }
-                            >
+                                onClick={ handleClickColour }
+                                >
 
-                        </div>
-                        { !picker ? null :
-                            <HuePicker
-                                className="playerTag__colourPicker"
+                            </div> */}
+                            <ColourPicker
                                 color={ colour }
                                 onChangeComplete={ handleChangeComplete }
-                            /> 
-                        }
-                    </div>
-                    <div className="playerTag__buttons">
-                        <Button 
-                            name="Save"
-                            whenClick={ handleSave }
-                            colour="green"
-                            classNamePassed="playerTag__button"
-                        />
-                        <Button 
-                            name="Delete"
-                            whenClick={ handleDelete }
-                            colour="red" 
-                            classNamePassed="playerTag__button"
-                        />
-                    </div>
+                            />
+                            { !picker ? null :
+                                <HuePicker
+                                    className="playerTag__colourPicker"
+                                    color={ colour }
+                                    onChangeComplete={ handleChangeComplete }
+                                /> 
+                            }
+                        </div>
+                        <div className="playerTag__buttons">
+                            <Button 
+                                name="Save"
+                                whenClick={ handleSave }
+                                colour="green"
+                                classNamePassed="playerTag__button"
+                            />
+                            <Button 
+                                name="Delete"
+                                whenClick={ handleDelete }
+                                colour="red" 
+                                classNamePassed="playerTag__button"
+                            />
+                        </div>
 
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
-    </>
+        </>
     );
 }
 export default PlayerTag;

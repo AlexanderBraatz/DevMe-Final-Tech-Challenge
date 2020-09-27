36745ee8da624f6a39155ba6e09ad1ba575ae180
Alexander Braatz <alex.braatz.g@gmail.com>
2020-09-27 23:29:49 +0100

    Updates

---
+++ precache-manifest.fba3b799451c3735b332c8db4fd3d668.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c080a3f356c12a8837f4465f3db4885c",
+    "url": "/DevMe-Final-Tech-Challenge/index.html"
+  },
+  {
+    "revision": "2458c8690b586df084d8",
+    "url": "/DevMe-Final-Tech-Challenge/static/css/main.551423fd.chunk.css"
+  },
+  {
+    "revision": "8d75381425cb65c1d175",
+    "url": "/DevMe-Final-Tech-Challenge/static/js/2.c27d3416.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DevMe-Final-Tech-Challenge/static/js/2.c27d3416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2458c8690b586df084d8",
+    "url": "/DevMe-Final-Tech-Challenge/static/js/main.06695a06.chunk.js"
+  },
+  {
+    "revision": "4543f8943c8bb05e6947",
+    "url": "/DevMe-Final-Tech-Challenge/static/js/runtime-main.8e00fc77.js"
+  },
+  {
+    "revision": "48419a412ae68ffb881ddb07b9d7e274",
+    "url": "/DevMe-Final-Tech-Challenge/static/media/Moonhouse-yE5M.48419a41.ttf"
+  },
+  {
+    "revision": "0faf379a55fbb697334a0c1cb885f443",
+    "url": "/DevMe-Final-Tech-Challenge/static/media/spaceScrollingBackground.0faf379a.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/DevMe-Final-Tech-Challenge/precache-manifest.fba3b799451c3735b332c8db4fd3d668.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DevMe-Final-Tech-Challenge/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});

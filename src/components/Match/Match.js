0cb876cf093579edb08bd9f0596a4f4b80d3fdf3
Alexander Braatz <alex.braatz.g@gmail.com>
2020-09-30 17:32:59 +0100

    Add Match styling

--- src/components/Match/Match.js
+++ src/components/Match/Match.js
@@ -1,89 +1,85 @@
-
 import React, {useState} from "react";
-// import Button from '../Button'
+import Button from '../Button'
 
 
 const Match = ({matches, matchPointer,players, roundCounter, endMatch}) => {
 
+    const [warning, setWarning]= useState(false);
     const [loser, setLoser]= useState(0);
     
     const handleSelection2 = () => setLoser(player2.id);
     const handleSelection1 = () => setLoser(player1.id);
     const findPlayerfromID = ( id ) => players.find((player)=> player.id === id);
-    const handleConfirmation = () => { if(loser !== 0) {endMatch(loser)}};
+    const handleConfirmation = () => { if(loser !== 0) {
+            endMatch(loser);
+            setLoser(0);
+            setWarning(false);
+        } else {
+            setWarning(true);
+        }
+    };
     
     let player1ID = matches[matchPointer][0];
     let player2ID = matches[matchPointer][1];
     let player1 = findPlayerfromID(player1ID);
     let player2 = findPlayerfromID(player2ID);
 
-    
-
-
-
-
 
 
     return(
         <>
-        <button
-            onClick={ handleSelection2 }
-        >{player1.name}</button>
-        <button
-            onClick={ handleSelection1 }
-        >{player2.name}</button>
-        <p>Match: {matchPointer + 1}</p>
-        <p>Round: {roundCounter}</p>
-        <button
-                    onClick={ handleConfirmation }
-                     >confirm Selection</button>
-        {/* <section 
-            className="start_container"
-            >
-            <div 
-                className="start__content"
-                >
-                <div 
-                    className="start__heading"
-                    >
-                    <h1 
-                        className="start__heading--content"
-                    >Matches ...</h1>
-
+        <section 
+            className="match__container">
+            <div
+                className="match__content">
+                <div>
+                    <div
+                        className="match__player_name"
+                        onClick={ handleSelection2 }>
+                        <p
+                            className="match__player_name--content"
+                            >{player1.name}</p>
+                    </div>
+                    <div 
+                        className="match__vs">
+                        <p
+                            className="match__vs--content"
+                            >V.S.</p>
+                    </div>
+                    <div
+                        className="match__player_name"
+                        onClick={ handleSelection1 }>
+                        <p
+                            className="match__player_name--content"
+                            >{player2.name}</p>
+                    </div>
                 </div>
-                {!matchView ? null :
-                    matches.map(({playing, winner})=>
-                        <>
-                            <div>
-                                <Button className="start__button"
-                                    name={playing[0]}
-                                    whenClick={ handleSelect(1) }
-                                    colour={ selected===1||winner===playing[0] ?
-                                        `red` : `green`} 
-                                    classNamePassed="start__button"
-                                />
-                                <Button className="start__button"
-                                    name={playing[1]}
-                                    whenClick={ handleSelect(2) }
-                                    colour={ selected===2||winner===playing[1] ?
-                                        `red` : `green`} 
-                                    classNamePassed="start__button"
-                                />
-                            </div>
-                            <Button className="start__button"
-                                name="CONFIRM and NEXT MATCH"
-                                whenClick={ handleConfirm }
-                                colour="red" 
-                                classNamePassed="start__button"
-                            />
-                        </>
-                    )
-
-                }
-                    
-
+                <div 
+                    className="match__match_and_round">
+                    <p 
+                        className="match__match_and_round--content"
+                        >Match : {matchPointer + 1}
+                    </p>
+                    <p 
+                        className="match__match_and_round--content"
+                        >Round : {roundCounter}
+                    </p>
+                </div>
+                <div>
+                    <Button 
+                        classNamePassed="match__confirm_button"
+                        name="Confirm + Next Match"
+                        whenClick={ handleConfirmation }
+                        colour= "red"
+                    />
+                    {!warning ? null :
+                        <div>
+                            <p>Please chose a winner first!</p>
+                        </div>
+                    }
+                </div>
             </div>
-        </section> */}
+        </section>
         </>
     )
 }
